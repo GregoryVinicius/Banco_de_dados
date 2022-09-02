2b75db1dfab2ac613eacff0a9bd44ac25f245338
GregoryVinicius <gregoryvinciuscla@gmail.com>
2022-09-02 15:06:07 -0300

    aula_2_curso

--- aula_2_curso.sql
+++ aula_2_curso.sql
@@ -22,3 +22,18 @@ INSERT INTO estado (nome,sigla) VALUES ('MINAS GERAIS', 'MG');
 
 SELECT * FROM estado;
 
+
+CREATE TABLE cidade (
+id INT NOT NULL AUTO_INCREMENT
+,nome VARCHAR(200) NOT NULL
+,ativo CHAR(1) NOT NULL DEFAULT 'S'
+,data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+,estado_id INT NOT NULL
+,CONSTRAINT pk_cidade PRIMARY KEY (id)
+,CONSTRAINT fk_cidade_estado FOREIGN KEY (estado_id) REFERENCES estado (id)
+,CONSTRAINT cidade_ativo_deve_ser_S_ou_N CHECK(ativo IN ('S', 'N'))
+,CONSTRAINT cidade_unica UNIQUE (nome, estado_id)
+);
+
+INSERT INTO cidade (nome, estado_id) VALUES ('CURITIBA', 1);
+SELECT * FROM cidade
